bb005b16db824c28b7909ed931d77229febe3a5e
BugRed <o.adrian.rodrigues@gmail.com>
2025-02-25 22:08:18 -0300

    Criação de sidebar, estilização da sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { Header } from "./components/Header"
-import {Post} from "./Post"
+import { Header } from "./components/Header/Header";
+import { Post } from "./Post";
+import styles from "./App.module.css";
 
-import "./global.css"
+import "./global.css";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div>
       <Header />
-      <Post author="Jão" content="Post do jão"/>
-      <Post author="Jão" content="Post do jão"/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Jão" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, rerum cum repellat, optio, sit odit nostrum laboriosam harum quidem voluptate accusamus quasi odio totam cumque? Rem illo saepe eius dolor." />
+          <Post author="Pedro" content="Legal!" />
+        </main> 
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
