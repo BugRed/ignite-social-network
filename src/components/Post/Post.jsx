cf73177c8b5255db764ad1603e8585d52db191e9
BugRed <o.adrian.rodrigues@gmail.com>
2025-03-06 12:12:36 -0300

    construção da area de commentarios

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -12,21 +12,35 @@ export function Post() {
           </div>
         </div>
 
-        <time title="03 de Março às 11:15h" datetime="2025-03-05 11:15:16">Publicado há 1h</time>
+        <time title="03 de Março às 11:15h" datetime="2025-03-05 11:15:16">
+          Publicado há 1h
+        </time>
       </header>
 
-        <div className={styles.content}>
+      <div className={styles.content}>
         <p>Fala galeraa 👋</p>
 
         <p>Acabei de subir mais um projeto no meu portifa.</p>
         <p>É um projeto que fiz no NLW Return, evento da Rocketseat.</p>
-        <p>O nome do projeto é DoctorCare 🚀 <a href="">devonlane.design</a></p>
-        <p>👉{' '}<a href=""> jane.design/doctorcare</a></p>
-        <p><a href="">#novoprojeto</a> {' '}
-         <a href="">#nlw </a> {' '}
-         <a href="">#rocketseat</a> </p>
-        </div>
+        <p>
+          O nome do projeto é DoctorCare 🚀 <a href="">devonlane.design</a>
+        </p>
+        <p>
+          👉 <a href=""> jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw </a>{" "}
+          <a href="">#rocketseat</a>{" "}
+        </p>
+      </div>
 
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
