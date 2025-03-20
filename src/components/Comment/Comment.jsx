d486e32198222e7498cb95acf9d0448126491e15
BugRed <o.adrian.rodrigues@gmail.com>
2025-03-20 15:14:46 -0300

    Criação da função de Aplaudir

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,13 +1,20 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "../Avatar/Avatar";
+import { useState } from "react";
 
 export function Comment({content, onDeleteComment}) {
+
+  const [likeCount, setlikeCount] = useState(0);
 
 
   function handleDeleteComment() {
     //comunicação com a tag parent
     onDeleteComment(content);
+  }
+
+  function handleLikeComment(){
+    setlikeCount(likeCount + 1)
   }
 
 
@@ -37,10 +44,12 @@ export function Comment({content, onDeleteComment}) {
         </div>
 
         <footer>
-          <button>
+
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
+
         </footer>
       </div>
     </div>
